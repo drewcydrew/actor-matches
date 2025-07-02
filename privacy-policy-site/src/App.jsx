dea776c487cec0c9de5a24a6cd1c2843d4885074
Andrew <andrew/jovaras@apac.bmt.org>
2025-07-03 01:21:04 +1000

    adding privacy policy page

---
+++ privacy-policy-site/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Privacy Policy</h1>
+      <p>This is the privacy policy for I Am DB...</p>
+      {/* Add your full text here */}
+    </div>
+  );
+}
+
+export default App;
+
